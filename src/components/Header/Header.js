9e0069491f7597587128dae9b57e9f3da4ca71dc
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-07-19 19:45:14 +0200

    Styling navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,34 +7,36 @@ function Header() {
   return (
     <header className={classes.header}>
 
-      {/* logo */}
       <div className={classes.header__content}>
+
+        {/* logo */}
         <h2 className={classes.header__content__logo}>car2you</h2>
+
+
+        {/* nav */}
+        <nav className={classes.header__content__nav}>
+          <ul role="list">
+            <li>
+              <a href="/">Link 1</a>
+            </li>
+            <li>
+              <a href="/">Link 2</a>
+            </li>
+          </ul>
+
+          {/* login/register */}
+          <button className={classes.header__content__profile}>
+            Link 3
+          </button>
+
+        </nav>
+
+        {/* hamburger icon */}
+        <div className={classes.header__content__toggle}>
+          <BiMenuAltRight />
+        </div>
+
       </div>
-
-      {/* nav */}
-      <nav className={classes.header__content__nav}>
-        <ul role="list">
-          <li>
-            <a href="/">Link 1</a>
-          </li>
-          <li>
-            <a href="/">Link 2</a>
-          </li>
-        </ul>
-      </nav>
-
-      {/* login/register */}
-      <button className={classes.header__content__profile}>
-        Link 3
-      </button>
-
-      {/* hamburger icon */}
-      <div className={classes.header__content__toggle}>
-        <BiMenuAltRight />
-      </div>
-
-
 
     </header>
   )
