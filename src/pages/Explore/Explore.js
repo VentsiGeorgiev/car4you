e3ac2d66e55d78007bd77c82286bfb050442cc6b
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-07-27 14:21:35 +0200

    Added Explore page / categories for rent and sell

--- src/pages/Explore/Explore.js
+++ src/pages/Explore/Explore.js
@@ -1,7 +1,31 @@
+import { Link } from 'react-router-dom';
+import rentACarImg from './../../assets/jpg/rentACar.jpg';
+import buyACarImg from './../../assets/jpg/buyACar.jpg';
+
+
 function Explore() {
   return (
-    <div>
+    <div className='container'>
       <h1>Explore</h1>
+      <h2>Categories</h2>
+      <div>
+
+        <div>
+          <Link to='/category/rent'>
+            <img src={rentACarImg} alt="Rent a car" />
+          </Link>
+          <h2>Rent a car</h2>
+        </div>
+
+        <div>
+          <Link to='/category/buy'>
+            <img src={buyACarImg} alt="Rent a car" />
+          </Link>
+          <h2>Buy a car</h2>
+        </div>
+
+      </div>
+
     </div>
   )
 }
