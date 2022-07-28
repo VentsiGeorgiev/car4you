9754ca02f7cc295467d4dd6610c823a5c9dd69fd
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-07-28 14:40:32 +0200

    Added Offers page

--- src/pages/Offers/Offers.js
+++ src/pages/Offers/Offers.js
@@ -1,7 +1,76 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { collection, getDocs, query, where, orderBy, limit } from 'firebase/firestore';
+import { db } from './../../firebase.config';
+import { toast } from 'react-toastify';
+import ListingItem from '../../components/ListingItem/ListingItem';
+
 function Offers() {
+  const [listings, setListings] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+
+  const params = useParams()
+
+  useEffect(() => {
+
+    const fetchListings = async () => {
+      try {
+        const listingRef = collection(db, 'listings')
+
+        const q = query(
+          listingRef,
+          where('offer', '==', true),
+          orderBy('timestamp', 'desc'),
+          limit(10)
+        )
+
+        const querySnap = await getDocs(q)
+
+        const listings = [];
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data()
+          })
+        })
+
+        setListings(listings)
+        setIsLoading(false)
+
+      } catch (error) {
+        toast.error("Error: Could Not Load Data");
+      }
+    }
+
+    fetchListings()
+
+  }, [params.categoryName])
+
+  console.log(listings);
+
+
   return (
-    <div>
-      <h1>Offers</h1>
+    <div className='container'>
+      <h2>
+        Offers
+      </h2>
+      {isLoading
+        ? <h3>Loading...</h3>
+        : listings && listings.length > 0
+          ? <>
+
+            <ul>
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  id={listing.id}
+                  listing={listing.data}
+                />
+              ))}
+            </ul>
+          </>
+          : <p>No Current Offers</p>
+      }
     </div>
   )
 }
