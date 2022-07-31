bb7c9c711374d4653c8a584c5991d93f8072e970
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-07-31 12:29:45 +0200

    Displaying listing details

--- src/pages/Listing/Listing.js
+++ src/pages/Listing/Listing.js
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { getDoc, doc } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { db } from '../../firebase.config';
+import { FaShareAlt } from 'react-icons/fa'
 
 function Listing() {
   const [listing, setListing] = useState(null)
   const [loading, setLoading] = useState(true)
+  const [shareLinkCopied, setShareLinkCopied] = useState(false)
 
   const navigate = useNavigate()
   const params = useParams()
@@ -27,8 +29,42 @@ function Listing() {
 
   }, [navigate, params.listingId])
 
+  if (loading) {
+    return <h3>Loading...</h3>
+  }
+
   return (
-    <div>Listing</div>
+    <div className="container">
+      <div onClick={
+        () => {
+          navigator.clipboard.writeText(window.location.href)
+          setShareLinkCopied(true)
+          setTimeout(() => {
+            setShareLinkCopied(false)
+          }, [2000])
+        }}>
+        <FaShareAlt />
+      </div>
+
+      {shareLinkCopied && <p>Link copied!</p>}
+
+      <div>
+        <p>For {listing.type === 'rent' ? 'Rent' : 'Sale'}</p>
+        <h3>{listing.make} {listing.model}</h3>
+        <p>Price: {listing.offer ? listing.discountedPrice : listing.regularPrice}</p>
+        {listing.offer && (
+          <p>Discount: {listing.regularPrice - listing.discountedPrice}</p>
+        )}
+        <p>Location: {listing.location}</p>
+        <ul>
+          <li>km: {listing.km}</li>
+          <li>insurance: {listing.insurance ? 'Yes' : 'No'}</li>
+        </ul>
+        {auth.currentUser?.uid !== listing.userRef && (
+          <Link to={`/contact/${listing.userRef}?listingName=${listing.make}&listingLocation=${listing.location}`} >Contact owner</Link>
+        )}
+      </div>
+    </div>
   )
 }
 
