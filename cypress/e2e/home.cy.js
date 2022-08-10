81bef2110d9ee0625ef8683d2ef07cc83c4cfb9d
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-08-10 15:37:48 +0200

    Correct rendering home page test

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,8 +1,19 @@
 describe('Explore page', () => {
-  it('Render explore page title', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000')
+  })
+
+  it('Render explore page title', () => {
     cy.get('h1')
       .should('exist')
       .contains('Find your next car')
+  })
+
+  it('Rent categorie is correct', () => {
+    cy.get('h2').eq(0).contains("Rent a car")
+  })
+
+  it('Sale categorie is correct', () => {
+    cy.get('h2').eq(1).contains("Buy a car")
   })
 })
